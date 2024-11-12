bf91f66c8628eb563971b98f31e11ec89126d128
Kevin_casta <kevincastano317509@correo.itm.edu.co>
2024-11-12 04:33:51 -0500

    Add roles
    Se agregaron roles y permisos de usuario

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,14 +27,14 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <img src="{{ asset('theme/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->full_name}}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>{{ Auth::user()->full_name}}</h6>
+              <span>{{ Auth::user()->role->name}}</span>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -43,38 +43,25 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
-                <span>My Profile</span>
+                <span>Mi perfil</span>
               </a>
             </li>
             <li>
               <hr class="dropdown-divider">
-            </li>
+
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                <i class="bi bi-question-circle"></i>
-                <span>Need Help?</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+                <form method="POST" action="{{ route('logout') }}" style="display: contents;">
+                    @csrf
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
+                    <button type="submit" class="dropdown-item d-flex aling-items-center">
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Cerrar Sesión</span>
+                    </button>
+
+                </form>
+
             </li>
 
           </ul><!-- End Profile Dropdown Items -->
