54fbf7ef50f5804f6210a279e5bdb1a6f5ba4074
Kevin_casta <kevincastano317509@correo.itm.edu.co>
2024-11-12 01:24:27 -0500

    Add breeze
    Se agrego la libreria breeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-include('web/productos.php');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/productos/create',[ProductosController::class, 'create'])->name('productos.create');
-Route::post('/productos/create',[ProductosController::class, 'store'])->name('productos.store');
-Route::get('/productos/edit/{id}',[ProductosController::class, 'edit'])->name('productos.edit');
-Route::put('/productos/edit',[ProductosController::class, 'update'])->name('productos.update');
-Route::delete('/productos/delete/{id}',[ProductosController::class, 'delete'])->name('productos.delete');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
